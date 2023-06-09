b04fcf9c5ec7f0a6e9de28fbdcf8e7c3bfc69678
Eric Fransen <efransen404@gmail.com>
2023-06-08 20:19:32 -0500

    Updated migrations

---
+++ NoteApi/Migrations/20230609011412_GuidChange.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NoteApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class GuidChange : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ID",
+                table: "Notes",
+                newName: "guid");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "guid",
+                table: "Notes",
+                newName: "ID");
+        }
+    }
+}
